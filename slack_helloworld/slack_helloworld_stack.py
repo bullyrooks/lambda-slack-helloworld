ec0f876301703ed9ba27544e56e856639a6729cd
brian <brian.rook@gmail.com>
2023-04-03 19:26:55 -0600

    feat: custom domain

--- slack_helloworld/slack_helloworld_stack.py
+++ slack_helloworld/slack_helloworld_stack.py
@@ -6,7 +6,9 @@ from aws_cdk import (
     aws_lambda as _lambda,
     aws_apigateway as apigateway,
     aws_iam as iam,
+    aws_certificatemanager as acm,
 )
+from aws_cdk.aws_apigateway import SecurityPolicy, EndpointType
 from aws_cdk.aws_ecr import Repository
 
 from constructs import Construct
@@ -22,6 +24,12 @@ class SlackHelloworldStack(Stack):
             repository=Repository.from_repository_name(self, "slack-helloworld-repo", "slack-helloworld"),
             tag_or_digest=image_tag
         )
+        slack_helloworld_domain_name = "slack-helloworld.bullyrooks.com"
+        # Create a certificate for the domain name
+        slack_helloworld_certificate = acm.Certificate.from_certificate_arn(
+            self, "hello-lambda_certificate", certificate_arn="arn:aws:acm:us-west-2:108452827623:certificate/c0f1a0ec-0566-449a-b0e5-217762f51b95")
+
+
         slack_lambda = _lambda.DockerImageFunction(
             scope=self,
             id="slack-helloworld-lambda",
@@ -47,8 +55,26 @@ class SlackHelloworldStack(Stack):
                                                         proxy=False,
                                                         )
 
+        slack_helloworld_api.add_domain_name(
+            id="slack-lambda-domain",
+            domain_name=slack_helloworld_domain_name,
+            certificate=slack_helloworld_certificate,
+            security_policy=SecurityPolicy.TLS_1_2,
+            endpoint_type=EndpointType.REGIONAL,
+        )
+
         # Add a POST method for the Slack bot
         slack_resource = slack_helloworld_api.root.add_resource("slack")
 
         # slack_wildcard_resource = slack_resource.add_resource("{proxy+}")
         slack_resource.add_method("POST")
+
+        # Create a deployment explicitly
+        deployment = apigateway.Deployment(self, "Deployment",
+                                      api=slack_helloworld_api)
+
+        # Map the deployment to the root URL
+        apigateway.Stage(self, "prod",
+                    deployment=deployment,
+                    stage_name="",
+                    rest_api=slack_helloworld_api)
