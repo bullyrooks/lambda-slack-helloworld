69f52746cb285f5ed29ea0c753beabb0344407ad
brian <brian.rook@gmail.com>
2023-04-04 19:24:01 -0600

    feat: call chatai lambda

--- slack_helloworld/slack_helloworld/slack_handler.py
+++ slack_helloworld/slack_helloworld/slack_handler.py
@@ -41,6 +41,16 @@ helloworldurlresponse = ssm.get_parameter(
     WithDecryption=False
 )
 helloworld_url = helloworldurlresponse["Parameter"]["Value"]
+chataiurlresponse = ssm.get_parameter(
+    Name='/prod/chatai/chatai.api.url',
+    WithDecryption=False
+)
+chatai_url = chataiurlresponse["Parameter"]["Value"]
+chataiapikeyresponse = ssm.get_parameter(
+    Name='/prod/chatai/lambda.api.key',
+    WithDecryption=True
+)
+chatai_api_key = chataiapikeyresponse["Parameter"]["Value"]
 
 
 # process_before_response must be True when running on FaaS
@@ -54,7 +64,7 @@ app = App(process_before_response=True,
 def handle_app_mentions(body, say, logger):
     logger.info("inside handle_app_mentions")
     logger.info(body)
-    say("What's up?")
+    say(call_chatai(body))
 
 @app.command("/hello")
 def respond_to_slack_within_3_seconds(ack, respond, command):
@@ -87,6 +97,24 @@ def call_hello_world(api_key, ):
     if response.status_code == 200:
         response_json = response.json()
         message = response_json["message"]
+        return message, response.status_code
+    else:
+        return None, response.status_code
+
+def call_chatai(text):
+    headers = {
+        'x-api-key': chatai_api_key,
+        'Content-Type': 'application/json',
+    }
+    logger("chatai request: %s", text)
+    response = requests.post(chatai_url,
+                             json={"text": text},
+                             headers=headers,
+                             )
+    logger("chatai response: %s", response)
+    if response.status_code == 200:
+        response_json = response.json()
+        message = response_json["body"]
         return message, response.status_code
     else:
         return None, response.status_code
